47b6aabe045e4c04350455ac7cfc565f73049cd6
Chen Chen <chenchen.bme@gmail.com>
2018-04-04 22:56:22 -0500

    new data and update in the scraper to fix HTML form tag issue

--- visa_scraper.py
+++ visa_scraper.py
@@ -4,7 +4,7 @@ Created on Sat Mar  4 18:50:16 2017
 
 @author: Chen Chen
 
-@version: 1.0
+@version: 1.1
 
 @license: MIT License
 """
@@ -14,7 +14,6 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 visaURL = 'http://checkee.info/main.php?dispdate={0:04d}-{1:02d}'
-tabWidth = 11
 
 # Get current time
 timestamp = time.time()
@@ -32,11 +31,11 @@ fileCSV.write('{0},{1}\n\n'.format(currentTime, totalEntries))
 
 # Write table headings
 fileCSV.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format(
-              'User ID', 
-              'User Name', 
-              'Visa Type', 
-              'Visa Entry', 
-              'City', 
+              'User ID',
+              'User Name',
+              'Visa Type',
+              'Visa Entry',
+              'City',
               'Major',
               'Visa Status',
               'Check Date',
@@ -45,24 +44,30 @@ fileCSV.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format(
               ))
 
 # Main Loop
-userID = 0
+caseID = 0
 for yr in range(2009,yearNow+1):
     for mo in range(1,13):
         if yr == yearNow and mo > monthNow:
             break
-        
+
         # Scrape a new page
         visaurl = visaURL.format(yr,mo)
         url = urllib.request.urlopen(visaurl)
         visapage = BeautifulSoup(url.read(), "lxml")
+
+        # Get table data by finding the largest table in the HTML
+        tabHTML = visapage('table', {'align' : 'center'})
+        tblLen = 0
+        for tbl in tabHTML:
+            if len(tbl) > tblLen:
+                tblLen = len(tbl)
+                tabData = tbl
         
-        # Get table data, only cleared visa entries will be collected
-        tabData = visapage('table')[7]
-        # Only get those green fields (completed cases)
+        # Only get the completed cases - those marked in green
         tabEntry = tabData('tr',{'bgcolor':'#00FF00'})
-        
+                                 
         print("Scraping Entry: {0}-{1:02d}, {2} records".format(yr,mo,len(tabEntry)))
-        
+
         for idx in range(len(tabEntry)):
             userName     = tabEntry[idx]('td')[1].text
             visaType     = tabEntry[idx]('td')[2].text
@@ -74,22 +79,22 @@ for yr in range(2009,yearNow+1):
             completeDate = tabEntry[idx]('td')[-2].text
             waitDays     = tabEntry[idx]('td')[-1].text
 
-            #print("UserID: {0:05d}, Visa Type: {1}".format(userID, visaType))
-            
+            #print("caseID: {0:05d}, Visa Type: {1}".format(caseID, visaType))
+
             fileCSV.write('{0:05d},{1},{2},{3},{4},{5},{6},{7},{8},{9}\n'.format(
-              userID,       # User ID
-              userName,     # User Name 
-              visaType,     # Visa Type 
-              visaEntry,    # Visa Entry 
-              city,         # City 
+              caseID,       # Case ID
+              userName,     # User Name
+              visaType,     # Visa Type
+              visaEntry,    # Visa Entry
+              city,         # City
               major,        # Major
               status,       # Visa Status
               checkDate,    # Check Date
               completeDate, # Complete Date
               waitDays      # Waiting Days
               ))
-            
-            userID += 1
+
+            caseID += 1
 
 fileCSV.close()
 print('All Done!')
